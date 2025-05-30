ab1dd5d375be7faf275520acd9e947cb9242a87d
Arhan <arhandonerkaya20@gmail.com>
2025-05-30 17:27:53 +0300

    lang=en translation added

--- main.py
+++ main.py
@@ -13,11 +13,30 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+   # weather_translation = {
+    #    "parçalı az bulutlu": "Partly Cloudy",
+     #   "sağanak": "Shower Rain",
+      #  "açık": "Clear",
+       # "az bulutlu": "Few Clouds",
+        #"kapalı": "Overcast",
+    #    "sis": "Fog",
+     #   "hafif yağmur": "Light Rain",
+      #  "yoğun yağmur": "Heavy Rain",
+       # "kar": "Snow",
+       # "gök gürültülü sağanak yağış": "Thunderstorm"
+        # gerektiği kadar ekleyebilirsin
+  #  }
+
 API_KEY = "1031abb06011708df56975abf0b04e6f"
 
 @app.get("/weather")
 def get_weather(city: str):
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric&lang=tr"
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric&lang=en"
     response = requests.get(url)
     data = response.json()
+
+   # description = data["weather"][0]["description"]
+   # translated = weather_translation.get(description.lower(), description)
+   # data["weather"][0]["description"] = translated
+
     return JSONResponse(content=data)
